dfe4d69c29d8a4a1cea0771c9ea2c70cdfbb5252
rtolefree1 <r_ryde2000@yahoo.com>
2023-04-26 17:10:11 -0400

    adding files to search and update using urls; products is not working

---
+++ routes/api/category-routes.js
@@ -0,0 +1,78 @@
+const router = require('express').Router();
+// const { canTreatArrayAsAnd } = require('sequelize/types/utils');
+const { Category, Product } = require('../../models');
+
+// The `/api/categories` endpoint
+
+router.get('/', async (req, res) => {
+  try{
+    // find all categories
+    const allCategories = await Category.findAll();
+     // be sure to include its associated Products --- DO I NEED THE NEXT LINE???
+   // include: [{model:Tag, through:product_tag, as: 'tag_product_tag'}]
+    res.status(200).json(allCategories);
+  }catch(err){
+    res.status(500).json(err);
+  }
+
+ 
+});
+
+router.get('/:id', async (req, res) => {
+  // find one category by its `id` value
+  try{
+    const categoryData = await Category.findByPk(req.params.id, {
+      // be sure to include its associated Products
+      include:[{model:Product}]
+      //include:[{model:Tag, through:product_tag, as: 'tag_product_tag'}]
+    });
+
+    if(!categoryData){
+      res.status(404).json({message: 'No Category found with this id'})
+    }
+    //else{
+      res.status(200).json(categoryData)
+    //}
+
+  }catch(err){
+    res.status(500).json(err);
+  }
+  
+})
+
+router.post('/', async (req, res) => {
+  // create a new category
+  try{
+      const categoryData = await Category.create(req.body);
+      res.status(200).json(categoryData);
+  }catch(err){
+    res.status(400).json(err);
+  }
+});
+
+router.put('/:id', (req, res) => {
+  // update a category by its `id` value
+
+  // const categoryData = await Category.
+});
+
+router.delete('/:id', async (req, res) => {
+  // delete a category by its `id` value
+  try{
+    const categoryData = await Category.destroy({
+      where: {
+        id: req.params.id
+      }
+    });
+
+    if(!categoryData){
+      res.status(404).json({message: 'No category found with this id'});
+      return;
+    }
+    res.status(200).json(categoryData)
+  }catch(err){
+    res.status(500).json(err);
+  }
+});
+
+module.exports = router;
